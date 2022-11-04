616025fddb1949737bdd499d80a9f80a085853e1
gontsemolefe <john.konene@gmail.com>
2022-11-04 23:02:22 +0200

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 -- counts and shows
-SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score DESC;
+SELECT score, COUNT(score) AS number 
+FROM second_table
+GROUP BY score 
+ORDER BY number DESC;
