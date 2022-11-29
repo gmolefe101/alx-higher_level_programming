eb0cdd5edc633a25402f1e0c161e3ee9f138451f
gmolefe101 <mrgmolefe101@gmail.com>
2022-11-29 12:33:17 -0800

    Task 8 updated

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let args = process.argv[2];
+const args = process.argv[2];
 if (isNaN(Number(args))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < Number(args); i++) {
+  for (const i = 0; i < Number(args); i++) {
     console.log('X'.repeat(args));
   }
 }
