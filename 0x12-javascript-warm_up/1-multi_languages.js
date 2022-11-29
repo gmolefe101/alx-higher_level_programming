6f26614e0bc3f6e1f1e1685121c4b4e3f80550db
gontsemolefe <john.konene@gmail.com>
2022-11-29 21:42:04 +0200

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const words = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for (let i = 0; i < words.length; i++) {
-  console.log(words[i])
-}
+
+console.log('C is fun')
+console.log('Python is cool')
+console.log('Javascript is amazing')
