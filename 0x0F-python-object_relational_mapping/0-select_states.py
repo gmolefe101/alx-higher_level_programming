a347e41c5d1605e18fd87fcb47b82a15feb53dfc
gontsemolefe <john.konene@gmail.com>
2022-12-04 22:41:02 +0200

    Update Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 
-def main(args):
-    # gets all state stuff
-    if len(args) != 4:
-        raise Exception("need 3 arguments!")
-    db = MySQLdb.connect(host='localhost',
-                         user=args[1],
-                         passwd=args[2],
-                         db=args[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
-    for state in states:
-        print(state)
-
-
+import MySQLdb
+from sys import argv
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-    main(sys.argv)
+
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host="localhost",
+                         port=3306)
+
+    cursor = db.cursor()
+    sort = "SELECT * FROM states ORDER BY states.id ASC"
+    cursor.execute(sort)
+    list_of_tuples = cursor.fetchall()
+    
+    for _tuple in list_of_tuples:
+        print(_tuple)
+
+    cursor.close()
+   
+    db.close()
