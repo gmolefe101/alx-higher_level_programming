7675a63071c517169a3b2f2c83b3f5ce256a4536
gontsemolefe <john.konene@gmail.com>
2022-12-05 01:57:25 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """Lists all cities from the database."""
 
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-
-    db = MySQLdb.connect(user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         host="localhost",
-                         port=3306)
-    cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name\
-    FROM cities\
-    JOIN states\
-    ON state_id=states.id\
-    ORDER BY cities.id ASC")
-    list_of_tuples = cursor.fetchall()
-    for _tuple in list_of_tuples:
+        db = MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                db=sys.argv[3],
+                host="localhost",
+                port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT cities.id, cities.name, states.name\
+                        FROM cities\
+                        JOIN states\
+                        ON state_id=states.id\
+                        ORDER BY cities.id ASC")
+        list_of_tuples = cursor.fetchall()
+        for _tuple in list_of_tuples:
             print(_tuple)
-    cursor.close()
-    db.close()
+        cursor.close()
+        db.close()
