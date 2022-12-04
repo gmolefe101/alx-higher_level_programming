25b4336904d13fc2fd12073218ea29e62d3ab310
gontsemolefe <john.konene@gmail.com>
2022-12-04 23:39:05 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3])
+        db=sys.argv[3],
+        port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
     states = cursor.fetchall()
