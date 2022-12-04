3c9502b0465fb17fedb1df0da05305f9770f4fdf
gontsemolefe <john.konene@gmail.com>
2022-12-04 22:53:46 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,8 +13,6 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states`")
     [print(state) for state in cursor.fetchall()]
-    
