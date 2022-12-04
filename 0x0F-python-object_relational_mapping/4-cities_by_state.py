b2a5f35bf8a9a085ea688dffa8f6203fb5b794eb
gmolefe101 <mrgmolefe101@gmail.com>
2022-12-04 15:45:40 -0800

    Tasks Pushed

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all cities from the database."""
+
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         host="localhost",
+                         port=3306)
+    """Connect to a MySQL server."""
+
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name\
+    FROM cities\
+    JOIN states\
+    ON state_id=states.id\
+    ORDER BY cities.id ASC")
+
+    list_of_tuples = cursor.fetchall()
+    for _tuple in list_of_tuples:
+            print(_tuple)
+
+    cursor.close()
+    db.close()
