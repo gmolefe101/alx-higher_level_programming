3cd4422c3d23731f1a87f38ae98a872424a17054
gontsemolefe <john.konene@gmail.com>
2022-12-05 01:49:34 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,15 +10,13 @@ if __name__ == "__main__":
                          db=argv[3],
                          host="localhost",
                          port=3306)
-    """Connect to a MySQL server."""
-
     cursor = db.cursor()
     cursor.execute("SELECT cities.id, cities.name, states.name\
     FROM cities\
     JOIN states\
     ON state_id=states.id\
     ORDER BY cities.id ASC")
-
+    
     list_of_tuples = cursor.fetchall()
     for _tuple in list_of_tuples:
             print(_tuple)
