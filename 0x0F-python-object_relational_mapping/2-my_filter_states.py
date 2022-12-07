f1e82aea5f48d66437e5d78f6c8069453c9ad03b
gontsemolefe <john.konene@gmail.com>
2022-12-07 21:19:23 +0200

    Update remaining Tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
-"""
-Lists all values in the states tables of a database where name
-matches the argument
-
-"""
-import sys
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
+from sys import argv
+
+def get_dbase():
+    """Takes arguments argv and list from database
+    Arguments:
+        argv[1]: mysql username
+        argv[2]: mysql password
+        argv[3]: database name
+    """
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            charset="utf8"
+                            )
+
+    # Getting a cursor
+    dbase_cur = dbase.cursor()
+
+    # Esecuting dbase queries
+    dbase_cur.execute("SELECT * FROM states WHERE BINARY\
+            name='{:s}' ORDER BY id ASC".format(argv[4]))
+
+    # Fetches all the rows of a query result
+    query_rows = dbase_cur.fetchall()
+
+    # Print result one by one
+    for rows in query_rows:
+        print(rows)
+
+    dbase_cur.close()
+    dbase.close()
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306)
-    cursor = db.cursor()
-    cursor.execute(f"SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS = {sys.argv[4]};")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+    get_dbase()    
